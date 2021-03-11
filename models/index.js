21dcaecc89b754dc82a76e94ea204ed0f98140b2
Amita <panwar.amita@gmail.com>
2021-03-11 00:18:05 -0500

    update

--- models/index.js
+++ models/index.js
@@ -7,10 +7,9 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.js')[env];
 var db        = {};
-console.log(config);
 if (config.use_env_variable) {
   
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable],config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
