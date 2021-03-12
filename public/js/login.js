75691b6d5178ebd696922799d5d7f54ea8da9f51
Amita <panwar.amita@gmail.com>
2021-03-11 20:19:09 -0500

    add

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,6 @@ $(document).ready(() => {
     if (!userData.email || !userData.password) {
       return;
     }
-    console.log("asdsdsdsd");
     // If we have an email and password we run the loginUser function and clear the form
     loginUser(userData.email, userData.password);
     emailInput.val("");
